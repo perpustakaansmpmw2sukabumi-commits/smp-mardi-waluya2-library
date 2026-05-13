2c88fc6fa6bf651922b3b7ab2a926494fc6ad9dd
perpustakaansmpmw2sukabumi-commits <perpustakaansmpmw2sukabumi@gmail.com>
2026-05-13 10:43:42 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      animation: {
+        'float': 'float 6s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translate3d(0,0,0)' },
+          '50%': { transform: 'translate3d(0,-10px,0)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
